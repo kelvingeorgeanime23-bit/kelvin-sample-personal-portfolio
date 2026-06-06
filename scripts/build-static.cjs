86e96d52e87eb643cf7ebc558e7daa19fe09ae75
Kelvin George <kelvingeorge.anime23@gamil.com>
2026-06-06 03:58:54 +0300

    Force client-side render by clearing TSR dehydrated state

--- scripts/build-static.cjs
+++ scripts/build-static.cjs
@@ -22,6 +22,10 @@ const html = `<!DOCTYPE html>
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=DM+Sans:wght@400;500;600&display=swap" rel="stylesheet">
   ${cssLink}
+  <script>
+    window.__TSR__ = window.__TSR__ || {};
+    window.__TSR__.dehydrated = undefined;
+  </script>
 </head>
 <body>
   <div id="root"></div>
